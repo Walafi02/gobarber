cf87a3f7373664ab4b5c9c421dcd43380c484436
Walafi02 <walafif@yahoo.com>
2019-10-16 06:06:57 -0300

    Corrigindo o nome da variavel userControler

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
-import userCOntroller from './app/controllers/UserController';
+import userController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/users', userCOntroller.store);
+routes.post('/users', userController.store);
 
 export default routes;
