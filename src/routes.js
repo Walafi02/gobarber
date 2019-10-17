d4c936635b12f977f9b377a30487dfd5b6c768b1
walafi <walafiferreira@trixlog.com>
2019-10-17 13:30:23 -0300

    Iniciando o modulo 03

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import userController from './app/controllers/UserController';
 import sessionController from './app/controllers/SessionController';
@@ -6,6 +8,7 @@ import sessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', userController.store);
 routes.post('/sessions', sessionController.store);
@@ -13,4 +16,9 @@ routes.post('/sessions', sessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', userController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
+
 export default routes;
